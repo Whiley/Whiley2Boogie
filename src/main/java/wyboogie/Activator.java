fe6be9368531b3dbd8dbabc930df288ab03b8d96
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-08-19 17:11:00 +1200

    Refactoring Activator

--- src/main/java/wyboogie/Activator.java
+++ src/main/java/wyboogie/Activator.java
@@ -14,18 +14,17 @@
 package wyboogie;
 
 import wycc.lang.Build;
+import wycc.lang.Filter;
 import wycc.util.AbstractCompilationUnit.Value;
 import wycli.cfg.Configuration;
 import wycli.lang.Command;
 import wycli.lang.Plugin;
 import wycc.lang.Content;
 import wycc.lang.Path;
-import wycc.util.Trie;
 import wyil.lang.WyilFile;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Collection;
 import java.util.List;
 
 import wyboogie.core.BoogieFile;
@@ -33,13 +32,13 @@ import wyboogie.tasks.BoogieTask;
 
 public class Activator implements Plugin.Activator {
 
-	public static Trie PKGNAME_CONFIG_OPTION = Trie.fromString("package/name");
-	public static Trie SOURCE_CONFIG_OPTION = Trie.fromString("build/whiley/target");
-	public static Trie TARGET_CONFIG_OPTION = Trie.fromString("build/boogie/target");
-	public static Trie VERIFY_CONFIG_OPTION = Trie.fromString("build/boogie/verify");
-	public static Trie VERBOSE_CONFIG_OPTION = Trie.fromString("build/boogie/verbose");
-	public static Trie DEBUG_CONFIG_OPTION = Trie.fromString("build/boogie/debug");
-	public static Trie TIMEOUT_CONFIG_OPTION = Trie.fromString("build/boogie/timeout");
+	public static Path PACKAGE_NAME = Path.fromString("package/name");
+	public static Path BUILD_WHILEY_TARGET = Path.fromString("build/whiley/target");
+	public static Path BUILD_BOOGIE_TARGET = Path.fromString("build/boogie/target");
+	public static Path BUILD_BOOGIE_VERIFY = Path.fromString("build/boogie/verify");
+	public static Path BUILD_BOOGIE_VERBOSE = Path.fromString("build/boogie/verbose");
+	public static Path BUILD_BOOGIE_DEBUG = Path.fromString("build/boogie/debug");
+	public static Path BUILD_BOOGIE_TIMEOUT = Path.fromString("build/boogie/timeout");
 	private static Value.UTF8 TARGET_DEFAULT = new Value.UTF8("bin".getBytes());
 
 	public static Command.Platform BOOGIE_PLATFORM = new Command.Platform() {
@@ -52,93 +51,59 @@ public class Activator implements Plugin.Activator {
 		@Override
 		public Configuration.Schema getConfigurationSchema() {
 			return Configuration.fromArray(
-					Configuration.UNBOUND_STRING(TARGET_CONFIG_OPTION, "Specify location for generated Boogie .bpl files", TARGET_DEFAULT),
-					Configuration.UNBOUND_BOOLEAN(VERIFY_CONFIG_OPTION, "Enable verification of Whiley files using Boogie", new Value.Bool(true)),
-					Configuration.UNBOUND_BOOLEAN(VERBOSE_CONFIG_OPTION, "Enable verbose output", new Value.Bool(false)),
-					Configuration.UNBOUND_BOOLEAN(DEBUG_CONFIG_OPTION, "Enable debug mode", new Value.Bool(false)),
-					Configuration.UNBOUND_INTEGER(TIMEOUT_CONFIG_OPTION, "Set timeout limit (s)", new Value.Int(10))
+					Configuration.UNBOUND_STRING(BUILD_BOOGIE_TARGET, "Specify location for generated Boogie .bpl files", TARGET_DEFAULT),
+					Configuration.UNBOUND_BOOLEAN(BUILD_BOOGIE_VERIFY, "Enable verification of Whiley files using Boogie", new Value.Bool(true)),
+					Configuration.UNBOUND_BOOLEAN(BUILD_BOOGIE_VERBOSE, "Enable verbose output", new Value.Bool(false)),
+					Configuration.UNBOUND_BOOLEAN(BUILD_BOOGIE_DEBUG, "Enable debug mode", new Value.Bool(false)),
+					Configuration.UNBOUND_INTEGER(BUILD_BOOGIE_TIMEOUT, "Set timeout limit (s)", new Value.Int(10))
 			);
 		}
 
 		@Override
-		public void initialise(Configuration configuration, Command.Project project) throws IOException {
-			Trie pkg = Trie.fromString(configuration.get(Value.UTF8.class, PKGNAME_CONFIG_OPTION).unwrap());
+		public Build.Task initialise(Path path, Command.Environment environment) throws IOException {
+			// Determine local configuration
+			Configuration config = environment.get(path);
+			// Get current repository snapshot
+			Build.SnapShot snapshot = environment.getRepository().last();
+			// Determine enclosing package name
+			Path pkg = Path.fromString(config.get(Value.UTF8.class, PACKAGE_NAME).unwrap());
+			// Identify directory where generated Boogie files are dumped.
+			Path source = Path.fromString(config.get(Value.UTF8.class, BUILD_WHILEY_TARGET).unwrap());
 			// Specify directory where generated Boogie files are dumped.
-			Trie source = Trie.fromString(configuration.get(Value.UTF8.class, SOURCE_CONFIG_OPTION).unwrap());
-			// Construct the source root
-			Path.Root sourceRoot = project.getRoot().createRelativeRoot(source);
-			// Register build target for this package
-			registerBuildTarget(configuration, project, sourceRoot, pkg);
-		}
-
-		private void registerBuildTarget(Configuration configuration, Build.Project project, Path.Root sourceRoot,
-				Trie pkg) throws IOException {
-			// Specify directory where generated JS files are dumped.
-			Trie target= Trie.fromString(configuration.get(Value.UTF8.class, TARGET_CONFIG_OPTION).unwrap());
+			Path target= Path.fromString(config.get(Value.UTF8.class, BUILD_BOOGIE_TARGET).unwrap());
 			// Determine whether verification enabled or not
-			boolean verification = configuration.get(Value.Bool.class, VERIFY_CONFIG_OPTION).unwrap();
+			boolean verification = config.get(Value.Bool.class, BUILD_BOOGIE_VERIFY).unwrap();
 			// Determine whether verbose output enabled or not
-			boolean verbose = configuration.get(Value.Bool.class, VERBOSE_CONFIG_OPTION).unwrap();
+			boolean verbose = config.get(Value.Bool.class, BUILD_BOOGIE_VERBOSE).unwrap();
 			// Determine whether debug mode enabled or not
-			boolean debug = configuration.get(Value.Bool.class, DEBUG_CONFIG_OPTION).unwrap();
+			boolean debug = config.get(Value.Bool.class, BUILD_BOOGIE_DEBUG).unwrap();
 			// Determine timeout to use
-			BigInteger timeout = configuration.get(Value.Int.class, TIMEOUT_CONFIG_OPTION).unwrap();
-			// Construct the binary root
-			Path.Root binaryRoot = project.getRoot().createRelativeRoot(target);
-			// Initialise the target file being built
-			Path.Entry<BoogieFile> binary = initialiseBinaryTarget(binaryRoot,pkg);
-			// Specify set of files included
-			Content.Filter<WyilFile> wyilIncludes = Content.filter("**", WyilFile.ContentType);
-			//
-			project.getRules().add(new AbstractBuildRule<WyilFile, BoogieFile>(sourceRoot, wyilIncludes, null) {
-				@Override
-				protected void apply(List<Path.Entry<WyilFile>> matches, Collection<Build.Task> tasks)
-						throws IOException {
-					// Construct a new build task
-					BoogieTask task = new BoogieTask(project, binary, matches.get(0));
-					// Configure task
-					task.setVerbose(verbose);
-					task.setVerification(verification);
-					task.setDebug(debug);
-					task.setTimeout(timeout.intValueExact());
-					// Submit the task for execution
-					tasks.add(task);
-				}
-			});
+			BigInteger timeout = config.get(Value.Int.class, BUILD_BOOGIE_TIMEOUT).unwrap();
+			// Construct build task
+			Filter includes = source.append(Filter.EVERYTHING);
+			// Identify all Whiley source files
+			List<WyilFile> sources = snapshot.getAll(WyilFile.ContentType, includes);
+			// Register build target for this package
+			return new BoogieTask(target.append(pkg), sources);
 		}
 
-		@Override
-		public Content.Type<?> getSourceType() {
-			return WyilFile.ContentType;
-		}
-
-		@Override
-		public Content.Type<?> getTargetType() {
-			return BoogieFile.ContentType;
-		}
-
-		@Override
-		public void execute(Build.Project project, Path.ID id, String method, Value... args) throws IOException {
-			throw new UnsupportedOperationException("Execute not supported");
-		}
-
-		private Path.Entry<BoogieFile> initialiseBinaryTarget(Path.Root binroot, Path.ID id) throws IOException {
-			Path.Entry<BoogieFile> target;
-			//
-			if(binroot.exists(id, BoogieFile.ContentType)) {
-				// Yes, it does so reuse it.
-				target = binroot.get(id, BoogieFile.ContentType);
-			} else {
-				// No, it doesn't so create and initialise it
-				target = binroot.create(id, BoogieFile.ContentType);
-			}
-			// Initialise with empty Boogie file
-			BoogieFile jsf = new BoogieFile();
-			// Write
-			target.write(jsf);
-			// Done
-			return target;
-		}
+//		private Path.Entry<BoogieFile> initialiseBinaryTarget(Path.Root binroot, Path.ID id) throws IOException {
+//			Path.Entry<BoogieFile> target;
+//			//
+//			if(binroot.exists(id, BoogieFile.ContentType)) {
+//				// Yes, it does so reuse it.
+//				target = binroot.get(id, BoogieFile.ContentType);
+//			} else {
+//				// No, it doesn't so create and initialise it
+//				target = binroot.create(id, BoogieFile.ContentType);
+//			}
+//			// Initialise with empty Boogie file
+//			BoogieFile jsf = new BoogieFile();
+//			// Write
+//			target.write(jsf);
+//			// Done
+//			return target;
+//		}
 	};
 
 	// =======================================================================
@@ -146,13 +111,13 @@ public class Activator implements Plugin.Activator {
 	// =======================================================================
 
 	@Override
-	public Module start(Module.Context context) {
+	public Plugin start(Plugin.Context context) {
 		// Register platform
 		context.register(Command.Platform.class, BOOGIE_PLATFORM);
 		// List of content types
 		context.register(Content.Type.class, BoogieFile.ContentType);
 		// Done
-		return new Module() {
+		return new Plugin() {
 			// what goes here?
 		};
 	}
@@ -162,7 +127,7 @@ public class Activator implements Plugin.Activator {
 	// =======================================================================
 
 	@Override
-	public void stop(Module module, Module.Context context) {
+	public void stop(Plugin module, Plugin.Context context) {
 		// could do more here?
 	}
 }
